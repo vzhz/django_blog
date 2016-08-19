7c4182bc890c9e77dec41a44369ed05d8069b110
vzhz <vehanus@gmail.com>
2016-08-19 16:48:23 -0400

    saving the form posts are written in

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
@@ -12,5 +12,12 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_new(request):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
     return render(request, 'blog/post_edit.html', {'form': form})
