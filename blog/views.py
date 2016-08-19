66900bc1fedfba6e9c118f1a7c31a8af0f7677e5
vzhz <vehanus@gmail.com>
2016-08-19 15:02:18 -0400

    link to page with form

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -9,3 +10,7 @@ def post_detail(request, pk):
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
