457321a04d9b39014e7da42609f499aa83f432ce
vzhz <vehanus@gmail.com>
2016-08-23 15:57:10 -0400

    add user logins

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import include, url
-from django.contrib import admin
+import djando.contrib.auth.views
 
-urlpatterns = [
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = ['',
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', django.contrib.auth.views.login, name='login'),
     url(r'', include('blog.urls')),
 ]
